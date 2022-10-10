4bbe9d6f49045cbfa0c2a1731f53cb8f5b6a1141
Giho Kang <kgh5554@naver.com>
2022-10-11 01:06:43 +0900

    221011

--- app/src/main/java/com/example/myku/SplashActivity.kt
+++ app/src/main/java/com/example/myku/SplashActivity.kt
@@ -1,12 +1,12 @@
 package com.example.myku
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
-    private val SPLASH_TIME_OUT:Long = 3000
+    private val SPLASH_TIME_OUT:Long = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
