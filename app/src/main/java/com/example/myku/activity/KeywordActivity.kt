d06b4a563acb2aa6f96455699ec7db50cf71237d
Giho Kang <kgh5554@naver.com>
2022-12-15 03:06:35 +0900

    최종

--- app/src/main/java/com/example/myku/activity/KeywordActivity.kt
+++ app/src/main/java/com/example/myku/activity/KeywordActivity.kt
@@ -16,6 +16,8 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import java.lang.Math.pow
+import kotlin.math.sqrt
 
 
 class KeywordActivity : AppCompatActivity() {
@@ -114,22 +116,84 @@ class KeywordActivity : AppCompatActivity() {
             .get()     // 문서 가져오기
             .addOnSuccessListener { result ->
 
-                //Toast.makeText(this,"잘 가져옴", Toast.LENGTH_LONG).show()
                 var temp_name = ""
                 var reco_num:Double = 0.0
                 var cnt:Int = 0
+
+                var WEIGHT_A:Double = 0.0
+                var WEIGHT_B:Double = 0.0
+
+                var array_slt = arrayOf(0.0,0.0,0.0,0.0,0.0,0.0)
+                var array_all = arrayOf(0.0,0.0,0.0,0.0,0.0,0.0)
+
  //성공할 경우
-                for (document in result) {  // 가져온 문서들은 result에 들어감
+                for (document in result) {
 
                     if(kw_field == document["field"] as String){
-                        //Toast.makeText(this,reco_num.toString(), Toast.LENGTH_SHORT).show()
 
 
                         if(temp_name == document["name"] as String){
+
+                            if(document["key1"] as String =="따뜻함"){
+                                array_all[0] += 1.0
+                            }else if(kw_key1=="시원함"){
+                                array_all[1] += 1.0
+                            }else if(kw_key1=="아늑함"){
+                                array_all[2] += 1.0
+                            }else if(kw_key1=="깨끗함"){
+                                array_all[3] += 1.0
+                            }else if(kw_key1=="조용함"){
+                                array_all[4] += 1.0
+                            }else if(kw_key1=="감성적임"){
+                                array_all[5] += 1.0
+                            }
+
                             if(kw_key1 == document["key1"] as String||kw_key1 == document["key2"]as String){
-                                reco_num+=(document["scope"] as Double)*0.8
+                                if(document["key1"] as String =="따뜻함"){
+                                    array_slt[0] += 1.0
+                                }else if(kw_key1=="시원함"){
+                                    array_slt[1] += 1.0
+                                }else if(kw_key1=="아늑함"){
+                                    array_slt[2] += 1.0
+                                }else if(kw_key1=="깨끗함"){
+                                    array_slt[3] += 1.0
+                                }else if(kw_key1=="조용함"){
+                                    array_slt[4] += 1.0
+                                }else if(kw_key1=="감성적임"){
+                                    array_slt[5] += 1.0
+                                }
+                                var arr_sum = 0.0
+                                var arr_inner = 0.0
+                                for(i in 0..5){
+                                    arr_inner += array_all[i]*array_slt[i]
+                                    arr_sum += pow(array_all[i],2.0)
+                                }
+                                WEIGHT_A = arr_inner/ (sqrt(arr_sum)*sqrt(2.0))
+                                //코사인 유사도 가중치 설정 가중치 A
+                                reco_num+=(document["scope"] as Double)*WEIGHT_A
                                 if(kw_key2 == document["key1"] as String||kw_key2 == document["key2"]as String){
-                                    reco_num+=(document["scope"] as Double)*1.3
+                                    if(document["key1"] as String =="따뜻함"){
+                                        array_slt[0] += 1.0
+                                    }else if(kw_key2=="시원함"){
+                                        array_slt[1] += 1.0
+                                    }else if(kw_key2=="아늑함"){
+                                        array_slt[2] += 1.0
+                                    }else if(kw_key2=="깨끗함"){
+                                        array_slt[3] += 1.0
+                                    }else if(kw_key2=="조용함"){
+                                        array_slt[4] += 1.0
+                                    }else if(kw_key2=="감성적임"){
+                                        array_slt[5] += 1.0
+                                    }
+                                    var arr_sum = 0.0
+                                    var arr_inner = 0.0
+                                    for(i in 0..5){
+                                        arr_inner += array_all[i]*array_slt[i]
+                                        arr_sum += pow(array_all[i],2.0)
+                                    }
+                                    WEIGHT_B = arr_inner/ (sqrt(arr_sum)*2.0)
+                                    //코사인 유사도 가중치 설정 가중치 B
+                                    reco_num+=(document["scope"] as Double)*WEIGHT_B
                                 }
                             }
                         }else{
@@ -141,8 +205,6 @@ class KeywordActivity : AppCompatActivity() {
                                 ml_long.add(document["longitude"].toString().toDouble())
                                 ml_rate.add(reco_num)
                             }
-                            //Toast.makeText(this,"추가됨", Toast.LENGTH_SHORT).show()
-                            //Toast.makeText(this,ml_name.size.toString(),Toast.LENGTH_SHORT).show()
                             reco_num = 0.0
 
                             temp_name = document["name"] as String
@@ -155,14 +217,8 @@ class KeywordActivity : AppCompatActivity() {
                             }
 
                         }
-                        //v.findViewById<TextView>(com.example.myku.R.id.cafeText1).setText(document["name"].toString())
-                        //v.findViewById<TextView>(com.example.myku.R.id.cafeText2).setText(document["address"].toString())
                     }
                     cnt++
-
-
-                    //val item = ListLayout(document["name"] as String, document["number"] as String)
-                    //itemList.add(item)
                 }
 
                 // 아이템 추가
